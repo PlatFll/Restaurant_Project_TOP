f9f3a4d5359b8de3546a8ffe4540651788f7e745
PlatFll <PlatooFl@gmail.com>
2026-06-28 22:00:55 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import "./styles.css";
 import { renderHome } from "./home.js";
+import { renderMenu } from "./menu.js";
 
 const content = document.querySelector("#content");
 
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const aboutBtn = document.querySelector("#about-btn");
+
+
 function clearContent() {
     content.textContent = "";
-}
+};
 
 function loadHome() {
     clearContent();
     renderHome(content);
-}
+};
+
+function loadMenu() {
+    clearContent();
+    renderMenu(content);
+};
 
 loadHome();
+
+homeBtn = addEventListener("click", loadHome);
+menuBtn = addEventListener("click", loadMenu);
---
+++ src/menu.js
@@ -0,0 +1,57 @@
+function itemGenerator(titleOfItem, picture, ingredients, price){
+    const itemCard = document.createElement("div");
+    itemCard.classList.add("item-card");
+
+    const itemTitle = document.createElement("h3");
+    itemTitle.textContent = titleOfItem;
+
+    const itemImage = document.createElement("img");
+    itemImage.src = picture;
+    itemImage.alt = titleOfItem;
+
+    const itemIngredients = document.createElement("p");
+    itemIngredients.textContent = ingredients;
+
+    const itemPrice = document.createElement("p");
+    itemPrice.textContent = price;
+
+    itemCard.appendChild(itemTitle);
+    itemCard.appendChild(itemImage);
+    itemCard.appendChild(itemIngredients);
+    itemCard.appendChild(itemPrice);
+
+    return itemCard;
+};
+
+
+function renderMenu (content) {
+    const menuPage = document.createElement("div");
+    menuPage.classList.add("menu-page");
+
+    const pageTitle = document.createElement("h1");
+    pageTitle.classList.add("page-title");
+    pageTitle.textContent = "Our Menu.";
+
+    menuPage.appendChild(pageTitle);
+
+    const firstItem = itemGenerator(
+        "Dragon Steak",
+        "",
+        "Grilled dragon meat, roasted potatoes, garlic butter",
+        "15 Silver Coins"
+    );
+
+    const secondItem = itemGenerator(
+        "Fisherman's Joy",
+        "",
+        "Seared sea bass, broccolli, sliced mushrooms",
+        "8 Silver Coins"
+    );
+
+    menuPage.appendChild(firstItem);
+    menuPage.appendChild(secondItem);
+
+    content.appendChild(menuPage);
+};
+
+export { renderMenu };
