4faf890ebcd708b806b1a81ee9cddfe49d12a10b
PlatFll <PlatooFl@gmail.com>
2026-06-29 06:00:46 +0300

    Split webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -6,29 +6,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devServer: {
-    static: "./dist",
-    open: true,
-  },
+
   module: {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ["style-loader", "css-loader"],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-    {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      type: "asset/resource",
-    },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
